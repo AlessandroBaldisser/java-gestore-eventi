95dd9b520afff8a259aa50aeb564622cb58afd25
AlessandroBaldisser <alessandro.baldisser.01@gmail.com>
2024-11-08 11:24:16 +0100

    ADD programmaeventi method

--- src/javaGestoreEventi/Main.java
+++ src/javaGestoreEventi/Main.java
@@ -1,39 +1,54 @@
 package javaGestoreEventi;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// Creo lo scanner 
-		Scanner scanner = new Scanner(System.in);
+//		// Creo lo scanner 
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		// Inizializzo la data dell'evento per inserirla nell'oggetto
+//		LocalDate dataEvento1 = LocalDate.of(2024, 12, 25);
+//		Evento evento1 = new Evento("Festa di Natale", dataEvento1, 150);
+//		
+//		// Chiedo all'utente quanti posti vuole prenotare 
+//		System.out.println("Vuole prenotare dei posti? Se si indicarne il numero: ");
+//		int numeroPostiPrenotati = scanner.nextInt();
+//		evento1.prenota(numeroPostiPrenotati);
+//		
+//		// Stampo numero di posti disponibili e numero di posti prenotati
+//		System.out.println("Numero di posti disponibili: " + evento1.getNumeroPostiDisponibili());
+//		System.out.println("Numero di posti prenotati: " + evento1.getNumeroPostiPrenotati());
+//		
+//		// Chiedo all'utente quanti posti vuole disdire
+//		System.out.println("Vuole disdire dei posti? Se si indicarne il numero: ");
+//		int numeroPostiDisdetti = scanner.nextInt();
+//		evento1.disdici(numeroPostiDisdetti);
+//		
+//		// Stampo numero di posti disponibili e numero di posti prenotati
+//		System.out.println("Numero di posti disponibili: " + evento1.getNumeroPostiDisponibili());
+//		System.out.println("Numero di posti prenotati: " + evento1.getNumeroPostiPrenotati());
+//		
+//		
+//		LocalDate dataConcerto1 = LocalDate.of(2024, 11, 13);
+//		LocalTime oraConcerto1 = LocalTime.of(21, 00);
+//		Concerto concerto1 = new Concerto("Titolo", dataConcerto1, 150, oraConcerto1, "19.99");
+//		System.out.println(concerto1.getPrezzo());
 		
-		// Inizializzo la data dell'evento per inserirla nell'oggetto
+		List<Evento> programmaEventi1 = new ArrayList<>();
 		LocalDate dataEvento1 = LocalDate.of(2024, 12, 25);
-		Evento evento1 = new Evento("Festa di Natale", dataEvento1, 150);
+		LocalDate dataEvento2 = LocalDate.of(2023, 12, 25);
+		LocalDate dataEvento3 = LocalDate.of(2022, 12, 25);
+		Evento evento1 = new Evento("titolo1", dataEvento1, 1650);
+		Evento evento2 = new Evento("titolo2", dataEvento2, 1651);
+		Evento evento3 = new Evento("titolo3", dataEvento3, 1652);
+		programmaEventi1.add(evento1);
+		programmaEventi1.add(evento2)
 		
-		// Chiedo all'utente quanti posti vuole prenotare 
-		System.out.println("Vuole prenotare dei posti? Se si indicarne il numero: ");
-		int numeroPostiPrenotati = scanner.nextInt();
-		evento1.prenota(numeroPostiPrenotati);
-		
-		// Stampo numero di posti disponibili e numero di posti prenotati
-		System.out.println("Numero di posti disponibili: " + evento1.getNumeroPostiDisponibili());
-		System.out.println("Numero di posti prenotati: " + evento1.getNumeroPostiPrenotati());
-		
-		// Chiedo all'utente quanti posti vuole disdire
-		System.out.println("Vuole disdire dei posti? Se si indicarne il numero: ");
-		int numeroPostiDisdetti = scanner.nextInt();
-		evento1.disdici(numeroPostiDisdetti);
-		
-		// Stampo numero di posti disponibili e numero di posti prenotati
-		System.out.println("Numero di posti disponibili: " + evento1.getNumeroPostiDisponibili());
-		System.out.println("Numero di posti prenotati: " + evento1.getNumeroPostiPrenotati());
-		
-		
-		LocalDate dataConcerto1 = LocalDate.of(2024, 11, 13);
-		LocalTime oraConcerto1 = LocalTime.of(21, 00);
-		Concerto concerto1 = new Concerto("Titolo", dataConcerto1, 150, oraConcerto1, "19.99");
-		System.out.println(concerto1.getPrezzo());
+		ProgrammaEventi programma1 = new ProgrammaEventi("titolo1", programmaEventi1);
+		programma1.aggiungiEvento(evento1);
 	}
 }
--- src/javaGestoreEventi/ProgrammaEventi.java
+++ src/javaGestoreEventi/ProgrammaEventi.java
@@ -1,6 +1,8 @@
 package javaGestoreEventi;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
 
 public class ProgrammaEventi {
 
@@ -15,6 +17,18 @@ public class ProgrammaEventi {
 	
 	public void aggiungiEvento(Evento evento) {
 		eventi.add(evento);
+	}
+	
+	public List<Evento> eventiInData(LocalDate giorno){
+		List<Evento> risultato = new ArrayList<>();
+		eventi.forEach(evento ->{    //il foreach andrà a ciclare ogni elemento nella lista, il "->" serve
+			//specificare il codice che dovrà eseguire per ogni elemento nella lista
+			if (evento.getData().equals(giorno)) {   //il metodo.equals andrà a confrontare la data dell'evento
+				//con la data da noi specificata
+				risultato.add(evento);  // se le date coincidono, allora aggiunge quell'elemento alla lista
+			}
+		});
+		return risultato;
 	}
 	
 	
